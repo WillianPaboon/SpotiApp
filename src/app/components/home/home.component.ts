f634a3cbaf880ee27e207abdc8a9d9202410a90e
willianPabon <willianpabon1293@gmail.com>
2023-03-04 17:05:31 -0500

    Desarrollo finalizado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { SpotifyService } from './../../services/spotify.service';
 import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +12,22 @@ export class HomeComponent {
 
   newSings: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: string = '';
 
   constructor( private spotifyService: SpotifyService, ) {
     this.loading = true;
+    this.error=false;
     this.spotifyService.getNewReleases()
     .subscribe( (data:any) => {
       console.log(data);
       this.newSings = data;
       this.loading = false;
+    },(errorService)=>{
+      this.error = true;
+      this.loading = false;
+      this.mensajeError = errorService.error.error.message;
+      console.log(errorService);
     });
    }
 
